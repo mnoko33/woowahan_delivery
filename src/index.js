8706f64a5c125860f4e3fed848210c2ee39844f6
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-09-29 22:34:41 +0900

    [new] redux-thunk 미들웨어 추가

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
-import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
+// import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
+import ReduxThunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
   <BrowserRouter>
