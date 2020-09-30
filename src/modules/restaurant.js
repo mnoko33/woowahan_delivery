b7b28c3e55d7f13442e9fa21fff03d2cc36772ef
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-09-30 21:57:02 +0900

    [new] 레스토랑 리덕스 추가
    1. 레스토랑 정보를 받아오는 액션
    2. 레스토랑 정보를 수정하는 액션

--- src/modules/restaurant.js
+++ src/modules/restaurant.js
@@ -2,8 +2,11 @@ import { createAction, handleActions } from 'redux-actions';
 import api from '../api';
 
 const SET_RESTAURANT_LIST = 'restaurant/SET_RESTAURANT_LIST';
+const SET_RESTAURANT_INFO = 'restaurant/SET_RESTAURANT_INFO'
 
 export const setRestaurantList = createAction(SET_RESTAURANT_LIST);
+export const setRestaurantInfo = createAction(SET_RESTAURANT_INFO);
+
 export const getRestaurantList = (categoryId) => dispatch => {
   return api.getRestaurantList(categoryId)
     .then(response => {
@@ -16,17 +19,47 @@ export const getRestaurantList = (categoryId) => dispatch => {
       console.log(error);
     })
 }
-
-const initialState = {
-  restaurantList: [],
+export const getRestaurantInfo = (restaurantId) => dispatch => {
+  return api.getRestaurantInfo(restaurantId)
+    .then(response => {
+      dispatch({
+        type: SET_RESTAURANT_INFO,
+        payload: response
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    })
 }
 
+export const updateRestaurantInfo = ({ id, data }) => dispatch => {
+  return api.updateRestaurantInfo({ id, data })
+    .then(response => {
+      console.log(response);
+      dispatch({
+        type: SET_RESTAURANT_INFO,
+        payload: response,
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    })
+}
+
+const initialState = {
+  restaurantInfo: null,
+  restaurantList: [],
+}
 
 const restaurantReducer = handleActions(
   {
     [SET_RESTAURANT_LIST]: (state, { payload: restaurantList }) => ({
       ...state,
-      restaurantList
+      restaurantList,
+    }),
+    [SET_RESTAURANT_INFO]: (state, { payload: restaurantInfo }) => ({
+      ...state,
+      restaurantInfo,
     })
   },
   initialState
