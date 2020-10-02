a0cee5b12558704f95a8586205f89aa3ecc9828f
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-10-02 19:15:52 +0900

    [new] 공용 컴포넌트 table 추가

---
+++ src/components/Table.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+  width: 100%;
+  margin-top: 10px;
+`;
+
+export const Tr = styled.tr`
+  min-height: 30px;
+`;
+
+export const Th = styled.th`
+  padding: 0;
+  width: 35%;
+  text-align: left;
+  margin-right: 3px;
+`;
+
+export const Td = styled.td`
+  padding: 0;
+  width: 65%;
+`;
