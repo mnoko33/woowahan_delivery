839b8aa73d047f22d58ff53486cc2ae8588fc1a1
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-10-02 19:15:41 +0900

    [refactoring] 공용 버튼 컴포넌트 style 적용 방법 수정
    기존 width: props => props.style.width로 적용하던 방식에서 styled-components의 css를 이용한 방법으로 수정

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const CustomButton = styled.button`
   all: unset;
-  width: ${props => props.style.width || '30px'};
-  height: ${props => props.style.height || '30px'};
-  color: ${props => props.style.color || null};
-  border: ${props => props.style.border || null};
-  border-radius: ${props => props.style.borderRadius || null};
-  background-color: ${props => props.style.backgrouncColor || null};
-  font-size: ${props => props.style.fontSize || null};
   &:hover {
     cursor: pointer;
+  }
+  ${(props) => 
+    props.style && css`
+      ${props.style}
+    `
   }
 `;
 
 function Button({ options, onClick }) {
-  const { icon, title, style } = options;
-
+  const { preIcon, postIcon, title, style } = options;
   return (
-    <CustomButton style={style || {}} onClick={onClick}>
-      { icon } { title }
+    <CustomButton style={style} onClick={onClick}>
+      { preIcon } { title } { postIcon }
     </CustomButton>
   )
 }
