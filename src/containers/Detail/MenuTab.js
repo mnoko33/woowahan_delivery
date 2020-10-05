6aa669363f0b4f6bff0b0f9530a89566ddf124d4
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-10-05 21:35:00 +0900

    [new] 매뉴를 클릭 시 모달을 띄우기 위한 기능 적용

--- src/containers/Detail/MenuTab.js
+++ src/containers/Detail/MenuTab.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { getMenuList } from '../../modules/restaurant';
 import MenuDrawer from '../../components/MenuDrawer';
+import MenuModal from '../../components/MenuModal';
 
 function MenuTab({ menuList, getMenuList, restaurantInfo }) {
+  const [selectedItemId, setSelectedItemId] = React.useState(null);
+
   React.useEffect(() => {
     getMenuList(restaurantInfo.id);
   })
+  
   return (
     <>
+      { selectedItemId && <MenuModal selectedItemId={selectedItemId} setSelectedItemId={setSelectedItemId} /> }
       { menuList.map((menu, index) => {
-        return <MenuDrawer key={index} menu={menu} />
+        return <MenuDrawer key={index} menu={menu} onClickItem={setSelectedItemId} />
       })}
     </>
   )
