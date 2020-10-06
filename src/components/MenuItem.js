1ff0a7e575f7fd4aba640c2d194abe5cbba03edd
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-10-07 01:52:29 +0900

    [refactoring] 메뉴 아이템 컴포넌트에서 배열 형태의 가격에 대응하면 부분을 숫자 가격 컴포넌트로 통일

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -8,35 +8,18 @@ const ItemWrapper = styled.div`
 `;
 
 function MenuItem({ item, onClick }) {
-  const renderPrice = () => {
-    if (isNaN(item.price)) {
-      const priceForMenuStrList = item.price.map(p => {
-        const [k, v] = Object.entries(p)[0];
-        return `${k}: ${commaizeNumber(v)}원`
-      })
-      return (
-        <ul style={{ margin: '5px 15px' }}>
-          {priceForMenuStrList
-              .map((priceForMenuStr, idx) => 
-                <li style={{ fontSize: '15px' }} key={idx}>
-                  {priceForMenuStr}
-                </li>)}
-        </ul>
-      )
-    } else {
-      return (
-        <span style={{ fontSize: '15px' }}>
-          {commaizeNumber(item.price) + '원'}
-        </span>
-      )
-    }
-  }
-
   return (
     <ItemWrapper onClick={e => onClick(e, item)}>
       <h4>{item.name}</h4>
-      { item.description.length > 0 && <h5 style={{ color: 'gray', fontWeight: 'lighter', margin: '5px auto' }}>{item.description}</h5> }
-      { renderPrice() }
+      { 
+        item.description.length > 0 && 
+        <h5 style={{ color: 'gray', fontWeight: 'lighter', margin: '5px auto' }}>
+          {item.description}
+        </h5> 
+      }
+      <span style={{ fontSize: '15px' }}>
+        {commaizeNumber(item.price) + '원'}
+      </span>
     </ItemWrapper>
   )
 }
