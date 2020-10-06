2e81f8993e7c9c0224b30fb80cd4adfbf944c9da
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-10-07 01:09:16 +0900

    [new] 새로운 액션과 기존 액션 수정
    1. ADD_ITEM : 기존 그냥 아이템을 추가하던 것에서 이미 존재하는 아이템은 갯수만 늘리는 방식으로 수정
    
    2. UPDATE_ITEM : 주문창에서 아이템의 갯수를 수정할 수 있도록 수정

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,11 +1,14 @@
 import { createAction, handleActions } from 'redux-actions';
+import { addExistingItem } from '../utils/addExistingItem';
 
 const ADD_ITEM = 'cart/ADD_ITEM';
 const REMOVE_ITEM = 'cart/REMOVE_ITEM';
+const UPDATE_ITEM = 'cart/UPDATE_ITEM';
 const RESET_CART = 'cart/RESET_CART';
 
 export const addItem = createAction(ADD_ITEM);
 export const removeItem = createAction(REMOVE_ITEM);
+export const updateItem = createAction(UPDATE_ITEM);
 export const resetCart = createAction(RESET_CART);
 
 const initialState = {
@@ -15,13 +18,19 @@ const initialState = {
 
 const cartReducer = handleActions(
   {
-    [ADD_ITEM]: (state, { payload: itemId }) => ({
+    [ADD_ITEM]: (state, { payload: newItem }) => ({
       ...state,
-      items: [ ...state.items, itemId ]
+      items: addExistingItem(state.items, newItem)
     }),
     [REMOVE_ITEM]: (state, { payload: itemId }) => ({
       ...state,
-      items: state.items.filter(id => id !== itemId)
+      items: state.items.filter(item => item.id !== itemId)
+    }),
+    [UPDATE_ITEM]: (state, { payload: { itemId, itemCnt } }) => ({
+      ...state,
+      items: state.items.map(item => {
+        return item.id === itemId ? { ...item, cnt: itemCnt } : item;
+      })
     }),
     [RESET_CART]: () => initialState,
   },
