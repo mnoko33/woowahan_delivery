504d19c3c74c15634cc156e6b61eebf57106b234
mnoko33 <46231900+mnoko33@users.noreply.github.com>
2020-10-07 20:39:25 +0900

    [refactoring] 기존 카트 컴포넌트에서 카트버튼 컴포넌트를 분리

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -1,40 +1,22 @@
 import React, { useState } from 'react';
-import { connect } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
-import Button from '../../components/Button';
-import { RiShoppingBasketLine } from 'react-icons/ri';
-import Counter from './Counter';
 import CartModal from '../../components/CartModal';
+import CartBtn from './CartBtn';
 
-function Cart({ items }) {
+function Cart() {
   const [isCartModalVisible, setIsCartModalVisible] = useState(false);
   const handleCartBtnClick = () => {
     setIsCartModalVisible(true);
   }
-  const options = {
-    style: {
-      width: '60px',
-      height: '60px',
-      borderRadius: '50%',
-      backgroundColor: '#2AC1BC',
-      textAlign: 'center',
-      color: '#FFF',
-      lineHeight: '20px',
-      fontSize: '35px',
-    },
-    preIcon: <RiShoppingBasketLine />
-  }
-
   const loaction = useLocation()
   const isCartShowed = loaction.pathname.startsWith('/restaurants')
   
-  if (items.length > 0 && isCartShowed) {
+  if (isCartShowed) {
     return (
       <Wrapper onClick={handleCartBtnClick}>
-        { isCartModalVisible && <CartModal visible={isCartModalVisible} setVisible={setIsCartModalVisible} /> }
-        <Button options={options} />
-        <Counter cnt={items.length} />
+        { isCartModalVisible && <CartModal visible={isCartModalVisible} setVisible={setIsCartModalVisible} />}
+        <CartBtn />
       </Wrapper>
     )
   } else {
@@ -42,11 +24,7 @@ function Cart({ items }) {
   }
 }
 
-export default connect(
-  state => ({
-    items: state.cartReducer.items,
-  })
-)(Cart);
+export default Cart;
 
 const Wrapper = styled.div`
   position: fixed;
